a45aea8ff4bff5e166f6b2a33676b59290435372
Guido Ortiz <guidoortiz93@hotmail.com>
2023-03-13 12:02:40 +0100

    csv generator

--- index.js
+++ index.js
@@ -4,11 +4,10 @@ const data = fs.readFileSync(
     './countries.txt',
     { encoding: 'utf8', flag: 'r' });
 
-var countries = data.split(/\r?\n/)
-countries.shift() 
+var countries = data.split(/\r?\n/) // ---> ['Country Population Area', 'Afghanistan 38,928,346 652,860', ....
+countries.shift() // --> ['Afghanistan 38,928,346 652,860', ....
 
-var countries_data = countries.map(c => c.split(" "))
-// console.log(countries_data);
+var countries_data = countries.map(c => c.split(" ")) // --> [ ['Afghanistan', '38,928,346', '652,860',], [], ....
 
 var regex = /[a-zA-Z]/;
 
@@ -19,15 +18,15 @@ var countries_density_population = countries_data.map(e => {
     var density = 0;
 
     for (var i = 0; i < e.length; i++) {
-        if (regex.test(e[i])){
+        if (regex.test(e[i])) {
             name = name.concat(e[i] + " ")
         }
-        else if (!regex.test(e[i])){
+        else if (!regex.test(e[i])) {
             numbers.push(parseFloat(e[i].replace(/,/g, "")))
             density = Number((numbers[0] / numbers[1]))
         }
     }
-    return{
+    return {
         country: name,
         population: numbers[0],
         area: numbers[1],
@@ -38,7 +37,10 @@ var countries_density_population = countries_data.map(e => {
 var countries_sorted = countries_density_population.sort((a, b) => b.density - a.density)
 
 var new_file = "COUNTRY POPULATION AREA DENSITY" + countries_sorted.map(e => {
-    return "\r\n".concat(e.country, " ").concat(e.population, " ").concat(e.area, " ").concat(e.density)
+    return (
+        "\r\n" 
+            .concat(e.country, " ").concat(e.population, " ").concat(e.area, " ").concat(e.density)
+    )
 })
 
 fs.writeFileSync("countries.csv", new_file);
